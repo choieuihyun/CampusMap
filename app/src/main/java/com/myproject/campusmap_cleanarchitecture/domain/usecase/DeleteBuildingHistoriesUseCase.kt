92d35ab4d4d43229dc56e12e21861d2bd4d5ca10
choieuihyun <amammmaa1@naver.com>
2022-11-30 19:16:07 +0900

    feature/ticket-014-SearchFragmentHistory 삭제 버튼 구현

---
+++ app/src/main/java/com/myproject/campusmap_cleanarchitecture/domain/usecase/DeleteBuildingHistoriesUseCase.kt
@@ -0,0 +1,14 @@
+package com.myproject.campusmap_cleanarchitecture.domain.usecase
+
+import com.myproject.campusmap_cleanarchitecture.domain.repository.BuildingRepository
+import javax.inject.Inject
+
+class DeleteBuildingHistoriesUseCase @Inject constructor(
+    private val repository: BuildingRepository
+) {
+
+    suspend operator fun invoke(id : Int) {
+        repository.deleteBuilding(id)
+    }
+
+}
--- app/src/main/java/com/myproject/campusmap_cleanarchitecture/ui/search/SearchFragmentViewModel.kt
+++ app/src/main/java/com/myproject/campusmap_cleanarchitecture/ui/search/SearchFragmentViewModel.kt
@@ -3,10 +3,8 @@ package com.myproject.campusmap_cleanarchitecture.ui.search
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.myproject.campusmap_cleanarchitecture.domain.model.Building
-import com.myproject.campusmap_cleanarchitecture.domain.usecase.AddBuildingHistoriesUseCase
-import com.myproject.campusmap_cleanarchitecture.domain.usecase.GetBuildingHistoriesUseCase
-import com.myproject.campusmap_cleanarchitecture.domain.usecase.GetBuildingsUseCase
-import com.myproject.campusmap_cleanarchitecture.domain.usecase.GetLectureRoomsUseCase
+import com.myproject.campusmap_cleanarchitecture.domain.model.BuildingHistory
+import com.myproject.campusmap_cleanarchitecture.domain.usecase.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -15,7 +13,8 @@ import javax.inject.Inject
 class SearchFragmentViewModel @Inject constructor(
     private val buildingsUseCase: GetBuildingsUseCase,
     private val buildingHistoryUseCase: GetBuildingHistoriesUseCase,
-    private val addBuildingHistoriesUseCase: AddBuildingHistoriesUseCase
+    private val addBuildingHistoriesUseCase: AddBuildingHistoriesUseCase,
+    private val deleteBuildingHistoriesUseCase: DeleteBuildingHistoriesUseCase
 ) : ViewModel() {
 
     val getBuildings = buildingsUseCase.invoke()
@@ -25,6 +24,12 @@ class SearchFragmentViewModel @Inject constructor(
     fun addBuildingHistories(building: Building) {
         viewModelScope.launch { // 왜 suspend를 안쓰는가에 대해서 -> Fragment단에서 Coroutine 안쓰려고? 쓰면 순서 꼬이잖아.
             addBuildingHistoriesUseCase.invoke(building)
+        }
+    }
+
+    fun deleteBuildingHistories(id : Int) {
+        viewModelScope.launch {
+            deleteBuildingHistoriesUseCase.invoke(id)
         }
     }
 
