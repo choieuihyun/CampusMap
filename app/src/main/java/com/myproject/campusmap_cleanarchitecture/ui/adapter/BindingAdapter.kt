ff791f5cd6faaefe4c4472e8ac5fb053ced26f76
choieuihyun <amammmaa1@naver.com>
2023-05-11 21:04:35 +0900

    refactor : 불필요한 코드 삭제

---
+++ app/src/main/java/com/myproject/campusmap_cleanarchitecture/ui/adapter/BindingAdapter.kt
@@ -0,0 +1,9 @@
+package com.myproject.campusmap_cleanarchitecture.ui.adapter
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("bind_load_url")
+fun ImageView.load(url: String) {
+
+}
